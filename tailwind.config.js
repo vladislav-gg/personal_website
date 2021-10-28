8eec5135d5ab430e2f0822778d981a100e77f26d
Vladislav <selies@mail.ru>
2021-10-28 03:24:49 +0100

    added favicon and purged unused css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const plugin = require('tailwindcss/plugin')
 
 
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ['../src/**/*.css", "./**/*.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     container: {
