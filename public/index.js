331cc450f128d0b0d9e56eacf392efeee543101e
Vladislav <selies@mail.ru>
2022-03-05 22:16:08 +0000

    burger menu added

--- public/index.js
+++ public/index.js
@@ -1,6 +1,13 @@
 // var dt = DateTime.now();
 // let currentTime = dt.toLocaleString(DateTime.DATETIME_MED);
 
-var now = new Date();
+let now = new Date();
 let localtime = now.toLocaleString();
 document.getElementById("time").innerHTML = localtime;
+
+const mobButton = document.querySelector("button.mobile-menu-button");
+const menu = document.querySelector(".mobile-menu");
+
+mobButton.addEventListener("click", () => {
+	menu.classList.toggle("hidden");
+});
