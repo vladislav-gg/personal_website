9fb5a806a6eaea9f9c1accc957eec093834a3d8d
Vladislav <selies@mail.ru>
2022-03-11 02:16:56 +0000

    fixed some styling issues

--- public/index.js
+++ public/index.js
@@ -1,9 +1,6 @@
-// var dt = DateTime.now();
-// let currentTime = dt.toLocaleString(DateTime.DATETIME_MED);
-
-let now = new Date();
-let localtime = now.toLocaleString();
-document.getElementById("time").innerHTML = localtime;
+// let now = new Date();
+// let localtime = now.toLocaleString();
+// document.getElementById("time").innerHTML = localtime;
 
 const mobButton = document.querySelector("button.mobile-menu-button");
 const menu = document.querySelector(".mobile-menu");
