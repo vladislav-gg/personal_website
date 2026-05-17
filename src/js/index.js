d5453bab1388dd588f6946f6b81150db54195e21
Vlad <vladislav.goian1@gmail.com>
2026-05-17 16:07:10 +0100

    migrated personal website to astro

---
+++ src/js/index.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+	const mobButton = document.querySelector("button.mobile-menu-button");
+	const menu = document.querySelector(".mobile-menu");
+	const iconHamburger = document.getElementById("icon-hamburger");
+	const iconClose = document.getElementById("icon-close");
+
+	mobButton.addEventListener("click", () => {
+		const isOpen = !menu.classList.contains("hidden");
+		menu.classList.toggle("hidden");
+		iconHamburger.classList.toggle("hidden");
+		iconClose.classList.toggle("hidden");
+		document.body.style.overflow = isOpen ? "" : "hidden";
+	});
+});
